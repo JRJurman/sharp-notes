a2d46989670184b7e6780b33e1f4e7228c454ca7
JRJurman <jrjurman@gmail.com>
2020-01-18 11:52:24 -0500

    better styles & fonts
    contributions by Ethan Jurman

--- src/components/FilterNav/FilterNav.js
+++ src/components/FilterNav/FilterNav.js
@@ -11,7 +11,7 @@ const html = registerHtml({
  * This component shows a list of filters for all notes.
  */
 export default () => {
-	const { notes, selectFilter } = useNotes()
+	const { notes, selectedFilter, selectFilter } = useNotes()
 
 	// get a flat list of filters for all the notes
 	const allFilters = notes.flatMap(note => note.filters)
@@ -19,16 +19,21 @@ export default () => {
 
 	// build list of links
 	const filterLinks = uniqueFilters.map(filter => {
+		// if this is the selected filter, give it a class
+		const isSelectedFilter = filter === selectedFilter
+		const filterLinkClass = isSelectedFilter ? 'SelectedFilter' : ''
+
 		const onSelectFilter = () => selectFilter(filter)
 		const filterWithoutParent = filter.split('/').slice(-1)
 		const filterLength = filter.split('/').length
-		return html`<NavItem onclick=${onSelectFilter} indent=${filterLength / 2}>${filterWithoutParent}</NavItem>`
+		return html`<NavItem class=${filterLinkClass} onclick=${onSelectFilter} indent=${filterLength / 2}>${filterWithoutParent}</NavItem>`
 	})
 
 	// '*' is a special filter that shows all notes
 	// for now we add a filter for 'All Notes' here,
 	// but this could be a different control in the future
 	const onSelectAll = () => selectFilter('*')
+
 	return html`
     <div class="FilterNav">
 			Filters
