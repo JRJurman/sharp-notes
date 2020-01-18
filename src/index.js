541ff1f8a2ff2113cd1ad390241ee613748cca20
JRJurman <jrjurman@gmail.com>
2020-01-18 11:17:04 -0500

    Remove color header, styles
    Co-authored-by: Ethan H. Jurman <ethanjurman@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import 'core-js/stable'
 import 'regenerator-runtime/runtime'
 import { registerHtml, start } from 'tram-one'
-import ColorHeader from './components/ColorHeader'
 import NoteArea from './components/NoteArea'
 import FilterNav from './components/FilterNav'
 import NoteNav from './components/NoteNav'
@@ -9,14 +8,13 @@ import { useIndexedNotes } from './hooks/useNotes'
 import './styles.scss'
 
 const html = registerHtml({
-	ColorHeader, NoteArea, FilterNav, NoteNav
+	NoteArea, FilterNav, NoteNav
 })
 
 const home = () => {
 	useIndexedNotes()
 	return html`
     <div class="Home">
-      <ColorHeader />
       <FilterNav />
 			<NoteNav />
 			<NoteArea />
